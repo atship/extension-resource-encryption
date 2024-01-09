0e5345e8cc69770f0a6b410177b7efdb296256c0
宋华林 <hualin@ivymobile.com>
2024-01-09 09:24:32 +0800

    init

---
+++ build.cmd
@@ -0,0 +1,2 @@
+java -jar bob.jar --platform=x86_64-win32 --verbose --build-artifacts=plugins clean build
+cp build/x86_64-win32/resourceencryption/pluginCustomResourceEncryption.jar resourceencryption/plugins/share
